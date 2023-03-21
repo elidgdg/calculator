1f8e893c5b80cf7c9191ba184539fa9c6adcb59c
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-03-21 13:33:28 +0000

    Add number button functionality

--- scripts.js
+++ scripts.js
@@ -18,6 +18,38 @@ function operate(operator, x, y) {
     return operator(x, y);
 }
 
+let num1, num2, operator;
 
+const displayDiv = document.getElementById("display");
 
+let displayValue = "";
 
+function updateDisplay(id) {
+    displayValue += id ;
+    displayDiv.innerHTML = displayValue;
+}
+
+let numBtns = document.querySelectorAll(".btn.num");
+numBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        updateDisplay(this.id);
+    });
+});
+
+function operatorClick(id) {
+    operator = id;
+    num1 = displayValue;
+    clearDisplay();
+}
+
+function clearDisplay() {
+    displayDiv.innerHTML = "0";
+    displayValue = "";
+}
+
+let operationBtns = document.querySelectorAll(".btn.operation");
+operationBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        operatorClick(this.id);
+    });
+});
